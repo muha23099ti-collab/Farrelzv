3e001d543d958c699509d73247f8d0bfc2fe23ab
farrelzvv <fzulviano@gmail.com>
2025-08-31 18:46:13 +0700

    config: next.config add webpack

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // PERBAIKAN: Menambahkan konfigurasi ini untuk mengatasi error Vercel
+  webpack: (config) => {
+    config.externals.push({
+      'troika-three-text': 'troika-three-text'
+    })
+    return config
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
