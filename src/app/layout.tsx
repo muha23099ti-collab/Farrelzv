a6cd6571da464794f18d0f9d9cf996a992d1dad3
farrelzvv <fzulviano@gmail.com>
2025-10-31 20:59:44 +0700

    Testing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,6 @@ export default function RootLayout({
         <PreloaderProvider> {/* Hanya PreloaderProvider yang membungkus */}
           <Preloader />
           <Header />
-          <SplashCursor />
           {children}
           <div className="background-gradient absolute inset-0 -z-50 max-h-screen" />
           <div className="pointer-events-none absolute inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-20 mix-blend-soft-light"></div>
