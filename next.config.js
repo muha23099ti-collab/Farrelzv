b03ca06e97cbdb71baed8a34b06a0a6a7419f84a
farrelzvv <fzulviano@gmail.com>
2025-11-18 21:06:17 +0700

    testing image carousel-fix-1

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
+// File: next.config.js
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Konfigurasi gambar Prismic
   images: {
     remotePatterns: [
       {
@@ -8,7 +11,20 @@ const nextConfig = {
       },
     ],
   },
+  
+  // Transpile paket 3D
   transpilePackages: ["three", "@react-three/drei", "troika-three-text"],
+
+  // --- INI YANG PENTING ---
+  // Kita atur webpack manual buat benerin bug import 'webgl-sdf-generator'
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      // Paksa pake file .js biasa, bukan yang .mjs yang bikin error
+      "webgl-sdf-generator": "webgl-sdf-generator/dist/webgl-sdf-generator.js",
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
